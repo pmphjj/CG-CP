e3ff50dedcbb2ecc3fa925b70e70824eb3e855d1
pmphjj <hjjgy365@163.com>
2019-03-15 10:22:27 +0800

    更新了变异分析的逻辑，已完成对于阶段内的变异的分析

--- processing_cp.py
+++ processing_cp.py
@@ -1,90 +1,69 @@
 # coding: utf-8
+from build_CP import Clinical_Pathway,Stages
+from Build_visits import Build_Visist_Order
 
 
-class ClinicalPathway:
-    #临床路径类
-    def __init__(self):
-        self.cp_id = -1
-        self.version_sqno = -1
-        self.cp_name = ""
-        self.stage_nums = 0 #阶段数目
-        self.stage = {} #阶段sqno ---> Stages类
-        self.score = 0.0
 
+def get_variation_of_visit(input_cp, input_visit):
+    """ 获取一个visit的变异情况
+    :param input_cp: Clinical_Pathway类
+    :param input_visit: Visit类
+    :return: 变异情况
+        分为两类：
+            1.必选没选
+            2.不在临床路径阶段内
 
-class Stages:
-    #临床路径阶段类
-    def __init__(self):
-        self.stage_sqno = -1
-        self.start_day = -1
-        self.end_day = -1
-        self.stage_desc = ""
-        self.stage_name = ""
-        self.stage_item_codes_set = set()#该阶段所有药物编号(CLINICAL_ITEM_CODES)的集合;
-        self.stage_orders_detail = dict()#对应的映射为【药物编号(CLINICAL_ITEM_CODES) ---> 药物医嘱类Detail_Order】 全局的dict？
-        self.score = 0.0
-
-class Order:
-    #医嘱父类
-    def __init__(self):
-        self.order_sqno = -1
-        self.order_class = ""
-        self.clinic_item_code = -1
-        self.perform_dept = ""
-        self.order_name = ""
-
-class DetailOrder(Order):
-    #医嘱子类，带具体执行时间等详细信息
-    def __init__(self):
-        super(DetailOrder,self).__init__()
-        self.order_confirm_time = ""
-
-class Visit():
-    def __init__(self):
-        self.visit_id = ""
-        self.orders_list = []
-        self.stage_2_day_dict = {}
-        self.day_2_stage_list = []
-
-def get_item_code(order):
-    #TODO
-    pass
-def get_stage_from_count(stage_count):
-    #TODO
-    pass
-
-def partition_orders(input_cp,input_visit):
-    item_code_2_stage_dict = {}
-    #生成医嘱代码与出现阶段的映射关系 {医嘱代码：set(出现阶段)}
-    for stage_index in input_cp.stage:
-        for item_code in input_cp.stage[stage_index].stage_item_codes_set:
-            if item_code not in item_code_2_stage_dict:
-                item_code_2_stage_dict[item_code] = set()
-                item_code_2_stage_dict[item_code].add(stage_index)
+    """
+    sort_visit_order_list = sorted(input_visit.day_level_info.items(), key=lambda x: x[0])
+    day_stage_map = []
+    #划分阶段 ，暂不考虑路径定义的阶段长度与具体执行日期间的差异
+    cur_stage_num = 0
+    max_stage_num = input_cp.stage_nums
+    for day_order in sort_visit_order_list:
+        temp_stage_num = cur_stage_num
+        #获取当天医嘱的编码集合
+        day_item_code_set = set([x["CLINIC_ITEM_CODE"] for x in day_order[1]])
+        while len(day_item_code_set) > 0 and temp_stage_num < max_stage_num:
+            temp_stage_code_set = input_cp.get_stage_code(temp_stage_num)
+            #当天剩余医嘱与当前阶段有交集，取其差集
+            if  day_item_code_set.isdisjoint(temp_stage_code_set) == True:
+                day_item_code_set = day_item_code_set.difference(temp_stage_code_set)
             else:
-                item_code_2_stage_dict[item_code].add(stage_index)
+                #无交集，判断与下一阶段是否有交集，若仍无交集，则视其为变异
+                if temp_stage_num == max_stage_num-1:
+                    break
+                temp_next_stage_code_set = input_cp.get_stage_code(temp_stage_num+1)
+                if day_item_code_set.isdisjoint(temp_next_stage_code_set) == True:
+                    day_item_code_set = day_item_code_set.difference(temp_next_stage_code_set)
+                    temp_stage_num += 1
+                else:
+                    break
 
-    cur_stage = 0
-    for day_order in input_visit.orders_list:
-        stage_count = {}
-        for order in day_order:
-            item_code = get_item_code(order)
-            if item_code in item_code_2_stage_dict:
-                for stage_index in item_code_2_stage_dict[item_code]:
-                    if stage_index < cur_stage or stage_index > cur_stage+1:
-                        continue
-                    if stage_index not in stage_count:
-                        stage_count[stage_index] = 1
-                    else:
-                        stage_count[stage_index] += 1
-        cur_order_stage = get_stage_from_count(stage_count)
-        #TODO 先确认一天是否可以划分为多个阶段
+        #若当天剩余医嘱集day_item_code_set不为空，则将其加入到当天的首个阶段的变异记录中
+        if len(day_item_code_set) > 0:
+            input_cp.add_variation_to_stage(day_item_code_set,cur_stage_num)
+        day_stage_map.append((cur_stage_num,temp_stage_num))
+        cur_stage_num = temp_stage_num
 
 
 
-    return input_visit
+
+def count_var(var_list):
+    #TODO 统计所有visit的变异情况
+    pass
+
+def update_var_info_of_cp(input_cp,stat_var):
+    #TODO 更新临床路径中记录的变异统计信息
+    pass
 
 if __name__ == "__main__":
-    input_cp = ClinicalPathway()
-    input_visit = Visit()
-    input_visit = partition_orders(input_cp,input_visit)
+    input_cp = Clinical_Pathway()
+    input_visits = Build_Visist_Order()
+
+    var_list = []
+    for visit in input_visits.all_visits_dict.values():
+        variation = get_variation_of_visit(input_cp, visit)
+        var_list.append(variation)
+    stat_var = count_var(var_list)
+    cp = update_var_info_of_cp(input_cp,stat_var)
+
